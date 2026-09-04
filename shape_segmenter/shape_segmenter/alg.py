2ad4a2d04523bf314deae8d9a7a94fbaa92a0dee
Gavin <gk2008wong@gmail.com>
2026-09-03 20:50:16 -0400

    Fixed offset caused by border padding. Outlines and centroids are highly accurate now.

--- shape_segmenter/shape_segmenter/alg.py
+++ shape_segmenter/shape_segmenter/alg.py
@@ -40,9 +40,10 @@ def alg(frame):
         dilated_image = cv2.dilate(edges, kernel, iterations=1)
 
         #Add padding around border to allow shapes on edge of screen to be segmented
+        pad = 4
         padded = cv2.copyMakeBorder(
             dilated_image, 
-            top=4, bottom=4, left=4, right=4, 
+            top=pad, bottom=pad, left=pad, right=pad, 
             borderType=cv2.BORDER_CONSTANT, 
             value=[255]
         )
@@ -51,7 +52,7 @@ def alg(frame):
         contours, _ = cv2.findContours(padded, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for cnt in contours:
-
+            cnt = cnt - np.array([pad, pad])  # undo the padding offset once, here
             area = cv2.contourArea(cnt)
 
             #Filter out noisy shapes
@@ -70,8 +71,8 @@ def alg(frame):
                     cv2.circle(frame, (c_u, c_v), 5, (0, 255, 0), -1)
 
                     #Calculate x & y coordinates utilizing the intrinsic matrix, z, and pixel coordinates
-                    c_x = c_u / f_x * c_z - 4
-                    c_y = c_v / f_y * c_z - 4
+                    c_x = c_u / f_x * c_z
+                    c_y = c_v / f_y * c_z
                     
                     #Truncate values to 2 decimal places
                     c_x = int(c_x * 100) / 100
